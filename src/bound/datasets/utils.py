447b28d64e85dacc25ed3f03a3ee723e686ddeb2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 05:27:44 +0000

    Adding tarfile member sanitization to extractall()

--- src/bound/datasets/utils.py
+++ src/bound/datasets/utils.py
@@ -113,7 +113,29 @@ def download_from_google_drive(dest: Path, gd_url: str, file_name: str,
     if file_name.endswith(".tar.gz"):
         if decompress:
             with tarfile.open(str(full_path), "r") as tar:
-                tar.extractall(path=str(dest))
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=str(dest))
     else:
         assert not decompress, "Cannot decompress a non tar.gz file"
 
